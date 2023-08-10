6d1ed3eb87e34d4a2afd8bf306f65cceaa7da6df
Pham Anh Minh <anhminh23252001@gmail.com>
2023-08-10 21:37:04 +0700

    finish user model

--- csharp-crud-api/Models/User.cs
+++ csharp-crud-api/Models/User.cs
@@ -2,5 +2,16 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Models
 {
-    [Table("users")]
+  [Table("users")]
+  public class User
+  {
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("name")]
+    public string Name { get; set; }
+
+    [Column("email")]
+    public string Email { get; set; }
+  }
 }
