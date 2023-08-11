8f7a5100b9fc093934efb27f132aaf8c4471af3b
phamm25 <122081592+phamm25@users.noreply.github.com>
2023-08-12 01:48:50 +0700

    get user controller

--- csharp-crud-api/Controllers/UsersController.cs
+++ csharp-crud-api/Controllers/UsersController.cs
@@ -7,3 +7,19 @@ namespace csharp_crud_api.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+  private readonly UserContext _context;
+
+  public UsersController(UserContext context)
+  {
+    _context = context;
+  }
+
+  // GET: api/users
+  [HttpGet]
+  public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+  {
+    return await _context.Users.ToListAsync();
+  }
+}
