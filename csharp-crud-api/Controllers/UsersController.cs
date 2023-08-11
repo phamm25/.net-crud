32d91371465406e318491df6042a7f6f5ea8a452
phamm25 <122081592+phamm25@users.noreply.github.com>
2023-08-12 01:47:07 +0700

    setting up controllers

---
+++ csharp-crud-api/Controllers/UsersController.cs
@@ -0,0 +1,9 @@
+using Data;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace csharp_crud_api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
